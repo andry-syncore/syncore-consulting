eb1cc9aab2b1b35aa0d5ecd76d975cb6ab6553c9
andrian syah <syncore.andriansyah@gmail.com>
2022-11-15 13:04:33 +0700

    move function tab active to custom js

--- public/assets/js/home/custom.js
+++ public/assets/js/home/custom.js
@@ -3,15 +3,37 @@ const dropdwonProgram = document.querySelector('.dropdown-program')
 const dropdownMenuProgram = document.querySelector('.dropdown-menu-program')
 const menuItemProgram = document.querySelectorAll('.menu-item-program')
 
-dropdwonProgram.addEventListener('click', (e) => {
+dropdwonProgram.addEventListener('click', function (e) {
    e.stopPropagation()
    dropdownMenuProgram.classList.toggle('show')
+   changeIcon(this)
 })
 
 menuItemProgram.forEach(item => item.addEventListener('click', function (e) {
    e.stopPropagation()
 }))
 
-document.addEventListener('click', (e) => {
+document.addEventListener('click', () => {
    dropdownMenuProgram.classList.remove('show')
+   changeIcon(dropdwonProgram)
 })
+
+const changeIcon = (dropdown) => {
+   if (dropdownMenuProgram.classList.contains('show')) {
+      dropdown.innerHTML = 'Program <i class="fa-solid fa-angle-up"></i>'
+   } else {
+      dropdown.innerHTML = 'Program <i class="fa-solid fa-angle-down"></i>'
+   }
+}
+
+// button tab portfolio
+const btnTabs = document.querySelectorAll('.btn-tab-portfolio')
+btnTabs.forEach((tab) => tab.addEventListener('click', function () {
+   // set active button when click
+   const activeClass = 'bg-orange-active'
+   const paddingClass = 'px-lg-5 mb-2 mb-md-0 px-3'
+
+   const currentActive = document.querySelector(`.${activeClass}`)
+   currentActive.className = currentActive.className.replace(`${activeClass} ${paddingClass}`, '')
+   this.className += ` ${activeClass} ${paddingClass}`
+}))
