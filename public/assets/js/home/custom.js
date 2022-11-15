889dc9a1c1abc4d0a256d322f5cd6df206fb9361
andrian syah <syncore.andriansyah@gmail.com>
2022-11-15 14:47:41 +0700

    add event listener dropdown menu program

--- public/assets/js/home/custom.js
+++ public/assets/js/home/custom.js
@@ -1,17 +1,14 @@
 // toggle dropdown menu program navbar
 const dropdwonProgram = document.querySelector('.dropdown-program')
 const dropdownMenuProgram = document.querySelector('.dropdown-menu-program')
-const menuItemProgram = document.querySelectorAll('.menu-item-program')
 
-dropdwonProgram.addEventListener('click', function (e) {
+dropdownMenuProgram.addEventListener('click', (e) => e.stopPropagation())
+
+dropdwonProgram.addEventListener('click', (e) => {
    e.stopPropagation()
    dropdownMenuProgram.classList.toggle('show')
    changeIcon(this)
 })
-
-menuItemProgram.forEach(item => item.addEventListener('click', function (e) {
-   e.stopPropagation()
-}))
 
 document.addEventListener('click', () => {
    dropdownMenuProgram.classList.remove('show')
