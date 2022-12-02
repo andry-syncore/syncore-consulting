54f48e820ad51ff3357e582798944ad9883a112f
andrian syah <syncore.andriansyah@gmail.com>
2022-12-02 11:07:18 +0700

    page media

--- resources/views/home/media/index.blade.php
+++ resources/views/home/media/index.blade.php
@@ -13,13 +13,13 @@
             <div class="row align-items-center py-5">
                <div class="col-md-5 col-lg-6">
                   <div class="text-md-start text-center">
-                     <h1 class="fw-600 text-orange">Portofolios</h1>
-                     <p>Berikut program - program yang telah kami jalankan</p>
+                     <h1 class="fw-600 text-orange">Media</h1>
+                     <p>Find & digest the horizons-expanding reads from all around the digital sphere.</p>
                   </div>
                </div>
                <div class="col-md-7 col-lg-6">
                   <div class="rounded-3 overflow-hidden">
-                     <img loading="lazy" src="{{ asset('storage/img/portfolio/placeholder-image.png') }}" alt="gambar hero" class="img-fluid">
+                     <img class="img-fluid" src="{{ asset('storage/img/media/image-hero.png') }}" alt="gambar hero" loading="lazy">
                   </div>
                </div>
             </div>
@@ -29,17 +29,17 @@
    <!-- end hero -->
 
    <!-- section content portfolio -->
-   <section id="portfolio" class="pb-5">
+   <section class="pb-5" id="portfolio">
       <div class="container">
-         <form action="" class="row">
+         <form class="row" action="">
             <div class="col-7 col-md-6 col-lg-9">
                <div class="input-group">
                   <span class="input-group-text bg-transparent"><i class="fa-solid fa-magnifying-glass text-orange"></i></span>
-                  <input type="text" class="border-start-0 form-control" id="search" name="search" placeholder="Cari portofolios">
+                  <input class="border-start-0 form-control" id="search" name="search" type="text" placeholder="Cari portofolios">
                </div>
             </div>
             <div class="col-5 col-md-3 col-lg-2">
-               <select class="form-select" name="sort" id="sort">
+               <select class="form-select" id="sort" name="sort">
                   <option>Sort by</option>
                   <option value="1">One</option>
                   <option value="2">Two</option>
@@ -48,20 +48,20 @@
             </div>
             <div class="col-12 col-md-3 col-lg-1 mt-md-0 mt-3">
                <div class="d-grid">
-                  <button type="submit" class="btn btn-orange">Search</button>
+                  <button class="btn btn-orange" type="submit">Search</button>
                </div>
             </div>
          </form>
 
-         <div class="mt-5 text-center d-none">
-            
-            <h5 class="fw-600 mb-0">Kalimantan</h5>
+         <div class="d-none mt-5 text-center">
+
+            <h5 class="fw-600 mb-0">Media</h5>
             <p class="text-dark-8 small mt-1">14 Pencarian ditemukan</p>
 
             <div class="d-flex align-items-center justify-content-center">
-               <img loading="lazy" src="{{ asset('storage/img/icon-search-not-found.svg') }}" alt="" class="img-fluid">
+               <img class="img-fluid" src="{{ asset('storage/img/icon-search-not-found.svg') }}" alt="" loading="lazy">
                <div class="mx-3 mt-4">
-                  <h5 class="fw-600 mb-0">Oops, Portofolios tidak ditemukan</h5>
+                  <h5 class="fw-600 mb-0">Oops, Media tidak ditemukan</h5>
                   <p class="small text-dark-8 mt-1">Yuk, Coba kata kunci lain untuk mendapatkan apa yang anda cari</p>
                </div>
             </div>
@@ -70,7 +70,7 @@
 
          <div class="row mt-5">
             <div class="col-12 col-md-4 col-lg-2 mb-lg-0 text-lg-center mb-1">
-               <button class="btn btn-tab-portfolio py-2 bg-orange-active px-lg-5 mb-2 mb-md-0 px-3">Semua</button>
+               <button class="btn btn-tab-portfolio bg-orange-active px-lg-5 mb-md-0 mb-2 py-2 px-3">Semua</button>
                <hr class="d-block d-md-none my-0">
             </div>
             <div class="col-12 col-md-4 col-lg-2 mb-lg-0 text-lg-center mb-1">
@@ -97,18 +97,91 @@
          <hr class="d-none d-md-block mt-3 mb-5">
 
          <div class="row mt-5">
-            @for ($i = 0; $i < 4; $i++)
-               <div class="col-md-6 mb-4">
+            <h3 class="text-orange fw-700 mb-3">Artikel</h3>
+            @for ($i = 0; $i < 6; $i++)
+               <div class="col-md-4 mb-4">
                   <div class="card card-body rounded-3 h-100 shadow-sm">
-                     <div class="rounded-3 overflow-hidden">
-                        <img loading="lazy" src="{{ asset('storage/img/portfolio/image-portfolio.png') }}" alt="gambar portfolio" class="img-fluid w-100">
+                     <div class="rounded-3 position-relative mb-2 overflow-hidden">
+                        <div class="bg-light position-absolute rounded-3 end-0 me-3 fw-500 top-0 mt-3 border bg-opacity-75 p-2 shadow">52 mins</div>
+                        <img class="img-fluid w-100" src="{{ asset('storage/img/media/image-article.png') }}" alt="gambar artikel" loading="lazy">
                      </div>
-                     <div class="alert alert-primary alert-fit-content fw-600 mt-3 border-0 py-2 px-4">Policy</div>
-                     <h4 class="card-title fw-600">Program Pendampingan Pengembangan Website Marketplace Belanja Kalteng</h4>
-                     <p class="text-dark-5 fw-500 mt-2">Merupakan program pendampingan yang bekerjasama dengan PT. Sari Husada</p>
+                     <div class="alert alert-primary alert-fit-content fw-600 mt-3 border-0 py-2 px-4">Lingkungan</div>
+                     <h5 class="card-title fw-600">BUMDes Riau Raih Omzet Rp 149 Miliar, Terbesar di Indonesia</h5>
 
-                     <div class="card-footer mb-3 border-0 bg-transparent px-0">
-                        <a href="" class="text-orange text-decoration-none fw-600">Selengkapnya <i class="fa-solid fa-arrow-right"></i></a>
+                     <div class="card-footer bg-transparent px-0">
+                        <div class="d-flex align-items-center">
+                           <div class="rounded-circle overflow-hidden">
+                              <img class="img-fluid" src="{{ asset('storage/img/homepage/image-user-article.png') }}" alt="gambar user" loading="lazy">
+                           </div>
+                           <div class="ms-2">
+                              <p class="fw-500 small m-0">Lana Steiner</p>
+                              <p class="text-secondary small m-0">18 Jan 2022</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            @endfor
+         </div>
+
+         <div class="row mt-5">
+            <h3 class="text-orange fw-700 mb-3">Audio</h3>
+            @for ($i = 0; $i < 6; $i++)
+               <div class="col-md-4 mb-4">
+                  <div class="card card-body rounded-3 h-100 shadow-sm">
+                     <div class="rounded-3 position-relative mb-2 overflow-hidden">
+                        <div class="bg-light position-absolute rounded-3 end-0 me-3 fw-500 top-0 mt-3 border bg-opacity-75 p-2 shadow">52 mins</div>
+                        <img class="img-fluid w-100" src="{{ asset('storage/img/media/placeholder-audio-image.png') }}" alt="gambar audio" loading="lazy">
+                     </div>
+                     <div class="d-flex align-items-center">
+                        <img src="{{ asset('storage/img/media/icon-play-audio.png') }}" alt="gambar audio" class="img-fluid me-2">
+                        <p class="fw-500 small text-dark-3 m-0">Cakap Startup Ep. 7: Aly Sangadji | Percakapan tentang Merantau, Menemukan Visi, Menggali Potensi</p>
+                     </div>
+                     <div class="alert alert-primary alert-fit-content fw-600 mt-3 border-0 py-2 px-4">Lingkungan</div>
+                     <h5 class="card-title fw-600">Cakap Startup Ep. 7: Aly Sangadji</h5>
+
+                     <div class="card-footer bg-transparent px-0 py-3">
+                        <div class="d-flex align-items-center">
+                           <div class="rounded overflow-hidden">
+                              <img class="img-fluid" src="{{ asset('storage/img/spotify.svg') }}" alt="icon spotify" loading="lazy">
+                           </div>
+                           <div class="ms-2">
+                              <p class="fw-500 text-dark-8 small m-0">Listen on</p>
+                              <p class="text-dark-3 fw-600 small m-0">Spotify</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            @endfor
+         </div>
+
+         <div class="row mt-5">
+            <h3 class="text-orange fw-700 mb-3">Video</h3>
+            @for ($i = 0; $i < 6; $i++)
+               <div class="col-md-4 mb-4">
+                  <div class="card card-body rounded-3 h-100 shadow-sm">
+                     <div class="rounded-3 position-relative mb-2 overflow-hidden">
+                        <div class="bg-light position-absolute rounded-3 end-0 me-3 fw-500 top-0 mt-3 border bg-opacity-75 p-2 shadow">52 mins</div>
+                        <img class="img-fluid w-100" src="{{ asset('storage/img/media/placeholder-video-image.png') }}" alt="gambar video" loading="lazy">
+                     </div>
+                     <div class="d-flex align-items-center">
+                        <img src="{{ asset('storage/img/media/icon-play-audio.png') }}" alt="gambar video" class="img-fluid me-2">
+                        <p class="fw-500 small text-dark-3 m-0">Cakap Startup Ep. 7: Aly Sangadji | Percakapan tentang Merantau, Menemukan Visi, Menggali Potensi</p>
+                     </div>
+                     <div class="alert alert-primary alert-fit-content fw-600 mt-3 border-0 py-2 px-4">Lingkungan</div>
+                     <h5 class="card-title fw-600">Cakap Startup Ep. 7: Aly Sangadji</h5>
+
+                     <div class="card-footer bg-transparent px-0 py-3">
+                        <div class="d-flex align-items-center">
+                           <div class="rounded overflow-hidden">
+                              <img class="img-fluid" src="{{ asset('storage/img/youtube.svg') }}" alt="icon youtube" loading="lazy">
+                           </div>
+                           <div class="ms-2">
+                              <p class="fw-500 text-dark-8 small m-0">Watch it on</p>
+                              <p class="text-dark-3 fw-600 small m-0">Youtube</p>
+                           </div>
+                        </div>
                      </div>
                   </div>
                </div>
